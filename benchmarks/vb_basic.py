d6656f2298de8116c7f7f397a5fd7d2508a1e8ef
chebee7i <chebee7i@gmail.com>
2014-11-30 15:31:59 -0600

    Update basic benchmark.

--- benchmarks/vb_basic.py
+++ benchmarks/vb_basic.py
@@ -11,11 +11,18 @@ from itertools import product
 outcomes = list(product(['0', '1', '2'], repeat=3))
 pmf = np.array([1/27] * 27)
 
+d = dit.Distribution(outcomes, pmf)
+
 """
 
 code = """
 d = dit.Distribution(outcomes, pmf)
 """
 
-bm = Benchmark(code, setup, name='initdist')
+bm1 = Benchmark(code, setup, name='initdist')
 
+code = """
+H = dit.shannon.entropy(d)
+"""
+
+bm2 = Benchmark(code, setup, name='entropy')
